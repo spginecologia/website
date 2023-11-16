3ff555362e3f27706238d17b0e75800f913e22b0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-11-16 00:43:28 +0000

    Bump

--- nextjs/app/[locale]/layout.js
+++ nextjs/app/[locale]/layout.js
@@ -11,16 +11,18 @@ export default async function Layout({ children, params: { locale } }) {
   //
 
   let messages;
+
   try {
     messages = (await import(`../../translations/${locale}.json`)).default;
   } catch (error) {
     notFound();
   }
 
-  //updateInterval={1}
   return (
     <NextIntlClientProvider locale={locale} messages={messages} timeZone="Europe/Lisbon" now={Date.now()}>
       {children}
     </NextIntlClientProvider>
   );
+
+  //
 }
