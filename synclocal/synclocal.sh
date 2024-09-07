52dc8beeb96ea514f2656aca790d6153284e125b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-07 20:59:09 +0100

    .

--- synclocal/synclocal.sh
+++ synclocal/synclocal.sh
@@ -3,5 +3,5 @@
 # # #
 
 echo "Starting 'websitedb' restore..."
-mongorestore --uri="mongodb://websitedbuser:websitedbpassword@websitedb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-websitedb-20240718182644"
+mongorestore --uri="mongodb://websitedbuser:websitedbpassword@websitedb/production?authSource=admin" --drop --preserveUUID --gzip --archive="spg-backup-websitedb-20240718182644"
 echo "Restore 'websitedb' complete!"
