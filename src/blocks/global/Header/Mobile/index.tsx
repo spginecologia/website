4f800f50327fc5342a9ebae755f745400075e767
Josue <jusi.monteiro@protonmail.com>
2024-10-29 01:02:57 +0000

    Fix: Mobile Nav Icon

--- src/blocks/global/Header/Mobile/index.tsx
+++ src/blocks/global/Header/Mobile/index.tsx
@@ -6,22 +6,35 @@ import Link from 'next/link';
 import styles from './styles.module.css';
 import { IoSearchSharp } from 'react-icons/io5';
 import { PiUserCircleFill } from 'react-icons/pi';
-import { Header } from '@/payload-types';
+import { Header, Media } from '@/payload-types';
 import { useState } from "react";
+import Image from 'next/image';
 
 export default function Mobile({ header }: { header: Header }) {
     const [linksMenu, setLinksMenu] = useState(false);
 
     return <>
-        <div className={styles.mobile}>
-            <Link href={'/academia/topicos/'}>
-                <IoSearchSharp size={35} />
-            </Link>
-            <Link href={'/account'}>
-                <PiUserCircleFill size={35} />
-            </Link>
-            <div onClick={() => setLinksMenu(true)}>
-                <IoMenuSharp size={35} />
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <Link href={'/'}>
+                    <Image
+                        alt={(header.logo as Media)?.alt ?? ""}
+                        height={40}
+                        src={(header.logo as Media)?.url ?? "/placeholder.png"}
+                        width={120}
+                    />
+                </Link>
+            </div>
+            <div className={styles.mobile}>
+                <Link href={'/academia/topicos/'}>
+                    <IoSearchSharp size={35} />
+                </Link>
+                <Link href={'/account'}>
+                    <PiUserCircleFill size={35} />
+                </Link>
+                <div onClick={() => setLinksMenu(true)}>
+                    <IoMenuSharp size={35} />
+                </div>
             </div>
         </div>
         {linksMenu &&
