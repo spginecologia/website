9aa3310e696e06196b75dfb8d413821e2856cd72
Josue <jusi.monteiro@protonmail.com>
2024-10-31 00:09:35 +0000

    Footer Init

--- src/blocks/global/Footer/index.tsx
+++ src/blocks/global/Footer/index.tsx
@@ -9,20 +9,34 @@ import { getPayloadHMR } from '@payloadcms/next/utilities';
 
 export default async function Component() {
 
-	const payload = await getPayloadHMR({config})
+	const payload = await getPayloadHMR({ config })
 	const footer = await payload.findGlobal({
 		slug: 'footer',
 	})
 
 	return (
 		<footer className={styles.footer}>
-			<div className={styles.column}>
-				<Image alt="Logotipo - Sociedade Portuguesa de Ginecologia" height={100} src={"/icons/spg-logo-white.svg"} width={100} />
+			<div className={styles.content}>
+				<Image alt="Logotipo - Sociedade Portuguesa de Ginecologia" height={100} src={"/icons/spg-logo-white.svg"} width={100} className={styles.logo} />
 				{/* <NewsletterForm /> */}
-				<div className={styles.contact}>
-					<Link href="tel:+351919494636">+351 919 494 636</Link>
-					<Link href="mailto:secretariao@spginecologia.pt">secretariao@spginecologia.pt</Link>
-					<Link href="https://www.spginecologia.pt">www.spginecologia.pt</Link>
+				<div></div>
+				<div className={styles.right}>
+					<div className={styles.contact}>
+						<Link className={styles.contactItem} href="tel:+351919494636">+351 919 494 636</Link>
+						<Link className={styles.contactItem} href="mailto:secretariao@spginecologia.pt">secretariao@spginecologia.pt</Link>
+						<Link className={styles.contactItem} href="mailto:academia@spginecologia.pt">academia@spginecologia.pt</Link>
+					</div>
+					<div className={styles.social}>
+						<Link href="https://www.facebook.com/spginecologia"><Image alt="Facebook" height={36} src={"/icons/facebook.svg"} width={36} /></Link>
+						<Link href="https://www.instagram.com/spginecologia"><Image alt="Instagram" height={36} src={"/icons/instagram.svg"} width={36} /></Link>
+						<Link href="https://www.linkedin.com/company/spginecologia"><Image alt="LinkedIn" height={36} src={"/icons/linkedin.svg"} width={36} /></Link>
+					</div>
+					<div className={styles.address}>
+						<div className={styles.addressTitle}>Sede Social</div>
+						<div>Edifício Cruzeiro 4 — 2º andar, sala 32</div>
+						<div>Largo Cruz de Celas — 3000-132 Coimbra</div>
+						<div>Portugal</div>
+					</div>
 				</div>
 			</div>
 		</footer>
