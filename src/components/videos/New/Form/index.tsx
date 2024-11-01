bc21a2ab1a6489332d0a5a30d36ee386b4b489bb
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-11-01 22:55:39 +0000

    feat: Enable create access for Media collection
    This commit enables the create access for the Media collection in the `Media.ts` file. This allows users to create new media items.
    
    Refactor: Update Next.js config
    
    This commit updates the Next.js config in the `next.config.mjs` file. It adds an experimental server action to increase the body size limit to 5gb.
    
    refactor: Restructure NewVideo component
    
    This commit restructures the `NewVideo` component in the `index.tsx` file. It improves the code formatting and organization.
    
    chore: Update package dependencies
    
    This commit updates the package dependencies in the `package.json` file. It adds new versions for `@lexical/react` and `react-hook-form` packages.
    
    refactor: Remove unused FormInput component
    
    This commit removes the unused `FormInput` component in the `FormInput/index.tsx` file.
    
    refactor: Update utils functions
    
    This commit updates the `slugify`, `formatSlug`, `capitalizeDate`, and `generateId` functions in the `utils.ts` file. It improves the code formatting and adds a new `generateId` function.
    
    refactor: Update Select component styles
    
    This commit updates the styles for the `Select` component in the `Select/index.tsx` file. It improves the code formatting and adds new CSS classes.
    
    refactor: Update Button component
    
    This commit updates the `Button` component in the `Button/index.tsx` file. It improves the code formatting and adds a new `disabled` prop.

--- src/components/videos/New/Form/index.tsx
+++ src/components/videos/New/Form/index.tsx
@@ -1,116 +1,242 @@
-"use client";
+'use client'
 
-import { useState } from "react";
-import RichTextEditor from "@/components/common/RichTextEditor";
-import FormInput from "../FormInput";
-import styles from "./styles.module.css";
-import { Media } from "@/payload-types";
-import Select from "@/components/common/Select";
-import Button from "@/components/common/Button";
-import { InterestArea } from "@/types/videos";
-import { handleSubmit } from "@/functions/handSubmit";
+import {
+  FormCheckbox,
+  FormInput,
+  FormRichTextEditor,
+  //   FormRichTextEditor,
+  FormSection,
+  FormSelect,
+  FormSubtitle,
+  FormTitle,
+} from '@/components/common/Form'
+import { useForm } from 'react-hook-form'
 
-const convertFileToMedia = (file: File): Media => {
-    return {
-        id: file.name,
-        alt: null,
-        updatedAt: new Date().toISOString(),
-        createdAt: new Date().toISOString(),
-        url: URL.createObjectURL(file),
-        filename: file.name,
-        mimeType: file.type,
-        filesize: file.size,
-        width: null,
-        height: null,
-        focalX: null,
-        focalY: null,
-    };
-};
+import styles from './styles.module.css'
+import Button from '@/components/common/Button'
+import { Video } from '@/payload-types'
+import { generateId } from '@/lib/utils'
+import { useState } from 'react'
 
-export function Form() {
-    const [title, setTitle] = useState("");
-    const [authors, setAuthors] = useState("");
-    const [file, setFile] = useState<Media | null>(null);
-    const [coverImage, setCoverImage] = useState<Media | null>(null);
-    const [introduction, setIntroduction] = useState("");
-    const [description, setDescription] = useState("");
-    const [interestArea, setInterestArea] = useState<InterestArea | null>(null);
-    const [declarationFile, setDeclarationFile] = useState<Media | null>(null);
-    const [isPrivacyChecked, setIsPrivacyChecked] = useState(false);
-    const [isSubmitting, setIsSubmitting] = useState(false);
+export default function VideoUploadForm() {
+  const { register, handleSubmit, watch } = useForm()
+  const [section, setSection] = useState('')
 
-    return (
-        <form className={styles.form} onSubmit={(event) => handleSubmit(event, title, authors, file, coverImage, introduction, description, interestArea, declarationFile, isPrivacyChecked, setIsSubmitting)}>
-            <div className={styles.title}>Submeter Vídeo</div>
-            <div className={styles.subtitle}>
-                Agradecemos o seu interesse em publicar na Academia SPG. Poderá encontrar as <a target="_blank" href="/academia/videos/new/help/">instruções de publicação aqui</a>. Para qualquer dúvida não hesite em contactar-nos para o email <a href="mailto:academia@spginecologia.pt">academia@spginecologia.pt</a>
-            </div>
-            <FormInput label="Título" placeholder="Introduza o nome do vídeo" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <FormInput label="Autores" placeholder="Nome Apelido; Nome Apelido; Nome Apelido; ..." type="text" value={authors} onChange={(e) => setAuthors(e.target.value)} />
-            <FormInput 
-                label="Ficheiro" 
-                placeholder="" 
-                type="file" 
-                onChange={(e) => {
-                    const selectedFile = e.target.files?.[0] || null;
-                    setFile(selectedFile ? convertFileToMedia(selectedFile) : null);
-                }} 
-            />
-            <FormInput 
-                label="Imagem de Capa" 
-                placeholder="" 
-                type="file" 
-                onChange={(e) => {
-                    const selectedFile = e.target.files?.[0] || null;
-                    setCoverImage(selectedFile ? convertFileToMedia(selectedFile) : null);
-                }} 
-            />
-            <div className={styles.aboutTitle}>Sobre este Vídeo</div>
-            <div className={styles.aboutSubtitle}>Introduza uma pequena introdução que descreva o core do vídeo. Na descrição pode incluir detalhes mais vastos.</div>
-            <div className={styles.textArea}>
-                <label>Introdução</label>
-                <textarea rows={8} value={introduction} onChange={(e) => setIntroduction(e.target.value)}></textarea>
-            </div>
-            <div className={styles.richTextEditor}>
-                <label>Descrição</label>
-                <RichTextEditor value={description} onChange={setDescription} />
-            </div>
-            <div className={styles.aboutTitle}>Metadados</div>
-            <div className={styles.aboutSubtitle}>Escolha vários tópicos que se relacionam com o tema do seu vídeo.</div>
-            <div className={styles.label}>Área de Interesse</div>
-            <Select
-                placeholder="— Selecione uma opção —"
-                data={[
-                    { value: "geral", label: "Geral" },
-                    { value: "colposcopia_patologia_tracto_genital_inferior", label: "Colposcopia Patologia Tracto Genital Inferior" },
-                    { value: "endoscopia_ginecologica", label: "Endoscopia Ginecológica" },
-                    { value: "ginecologia_oncologica", label: "Ginecologia Oncológica" },
-                    { value: "menopausa", label: "Menopausa" },
-                    { value: "uroginecologia", label: "Uroginecologia" },
-                ]}
-                value={interestArea}
-                onChange={(value) => setInterestArea(value as InterestArea)}
-            />
-            <div className={styles.aboutTitle}>Privacidade e Conflito de Interesses</div>
-            <div className={styles.aboutSubtitle}>
-                A SPG valoriza os direitos de privacidade e a garantia de não existirem quaisquer conflitos de interesse inerentes à publicação de um vídeo científico. Pedimos ao autor do vídeo que assine a <a target="_blank" href="https://spginecologia.pt/wp-content/mu-plugins/forms/video_submit/AcademiaSPG-DeclaracaoCedenciaDireitos.pdf">Declaração de Cedência de Direitos</a> e faça o upload do ficheiro no campo seguinte. Pode encontrar a <a target="_blank" href="https://spginecologia.pt/wp-content/mu-plugins/forms/video_submit/AcademiaSPG-DeclaracaoCedenciaDireitos.pdf">declaração aqui</a>.
-            </div>
-            <FormInput
-                label="Declaração Assinada"
-                placeholder=""
-                type="file"
-                onChange={(e) => {
-                    const selectedFile = e.target.files?.[0] || null;
-                    setDeclarationFile(selectedFile ? convertFileToMedia(selectedFile) : null);
-                }}
-            />
-            <label className={styles.checkboxWrapper}>
-                <input type="checkbox" checked={isPrivacyChecked} onChange={() => setIsPrivacyChecked(!isPrivacyChecked)} />
-                <div className={styles.checkboxText}>Confirmo que possuo os direitos de imagem dos ficheiros enviados e estão ocultas as respectivas identificações pessoais, de acordo com o <a target="_blank" href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:02016R0679-20160504">Regulamento Geral de Protecção de Dados (RGPD)</a>.</div>
-            </label>
-            <Button type="submit">
-                {isSubmitting ? "Submetendo..." : "Submeter Vídeo"}
-            </Button>
-        </form>
-    );
+  const onSubmit = async (data: any) => {
+    data.section = section
+    const files = [data.file[0], data.coverImage[0], data.declarationFile[0]]
+
+    //Upload files
+    const responses = await Promise.all(
+      files.map(async (file) => {
+        const formData = new FormData()
+        formData.append('file', file)
+
+        console.log('formData', file)
+
+        return fetch('/api/media', {
+          method: 'POST',
+          body: formData,
+        })
+      }),
+    )
+
+    if (responses.some((response) => !response.ok)) {
+      alert('Failed to upload files')
+      return
+    }
+
+    const responsesData = await Promise.all(responses.map((response) => response.json()))
+
+    console.log('responsesData', responsesData)
+
+    const video: Video = {
+      id: generateId(),
+      title: data.title,
+      video_featured: false,
+      video_rgpd_confirmation: data.isPrivacyChecked,
+      video_file_length: null,
+      video_file: responsesData[0].doc.id,
+      video_declaration_signed: responsesData[2].doc.id,
+      video_authors: data.authors,
+      video_section: data.section as
+        | 'geral'
+        | 'colposcopia_patologia_tracto_genital_inferior'
+        | 'endoscopia_ginecologica'
+        | 'ginecologia_oncologica'
+        | 'menopausa'
+        | 'uroginecologia',
+      video_introduction: data.introduction,
+      video_description: {
+        root: {
+          children: [
+            {
+              children: [
+                {
+                  detail: 0,
+                  format: 0,
+                  mode: 'normal',
+                  style: '',
+                  text: 'O caso descreve uma mulher de 32 anos com antecedentes de Infertilidade primária com 18 meses de evolução e com queixas de dismenorreia primária. A quistectomia laparoscópica pela técnica invertida prima por realizar o ',
+                  type: 'text',
+                  version: 1,
+                },
+                {
+                  detail: 0,
+                  format: 2,
+                  mode: 'normal',
+                  style: '',
+                  text: 'stripping ',
+                  type: 'text',
+                  version: 1,
+                },
+                {
+                  detail: 0,
+                  format: 0,
+                  mode: 'normal',
+                  style: '',
+                  text: 'na porção mais espessa do quisto reduzindo a possibilidade de fragmentar as paredes do mesmo, identificando assim mais facilmente o plano  de separação entre o parênquima ovárico e o quisto. Adicionalmente, a realização da incisão até aos bordos do quisto é uma referência importante de orientação para o cirurgião, realizando a quistectomia sob visualização direta, permitindo uma excisão completa. Neste caso, e por se tratar de um quadro de infertilidade associada à endometriose é aconselhada a realização da prova de cromotubação tubária bilateral no final do procedimento para confirmar a permeabilidade tubar.',
+                  type: 'text',
+                  version: 1,
+                },
+              ],
+              direction: null,
+              format: '',
+              indent: 0,
+              type: 'paragraph',
+              version: 1,
+              textFormat: 0,
+              textStyle: '',
+            },
+          ],
+          direction: null,
+          format: '',
+          indent: 0,
+          type: 'root',
+          version: 1,
+        },
+      },
+      author: '671192ffac28620f8ff592bf',
+      categories: [],
+      featured: responsesData[1].doc.id,
+      updatedAt: new Date().toISOString(),
+      createdAt: new Date().toISOString(),
+    }
+
+    console.log(video)
+
+    const response = await fetch('/api/videos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(video),
+    })
+
+    if (response.ok) {
+      alert('Video submitted successfully')
+    } else {
+      alert('Failed to submit video')
+    }
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      {/* Title and subtitle */}
+      <FormTitle>Upload a video</FormTitle>
+      <FormSubtitle>
+        Agradecemos o seu interesse em publicar na Academia SPG. Poderá encontrar as{' '}
+        <a target="_blank" href="/academia/videos/new/help/">
+          instruções de publicação aqui
+        </a>
+        . Para qualquer dúvida não hesite em contactar-nos para o email{' '}
+        <a href="mailto:academia@spginecologia.pt">academia@spginecologia.pt</a>
+      </FormSubtitle>
+      {/* Main fields */}
+      <FormInput
+        label="Título"
+        placeholder="Introduza o nome do vídeo"
+        type="text"
+        {...register('title')}
+      />
+      <FormInput
+        label="Autores"
+        placeholder="Nome Apelido; Nome Apelido; Nome Apelido; ..."
+        type="text"
+        {...register('authors')}
+      />
+      <FormInput label="Ficheiro" placeholder="" type="file" {...register('file')} />
+      <FormInput label="Imagem de Capa" placeholder="" type="file" {...register('coverImage')} />
+      {/* About fields */}
+      <FormSection
+        title={'Sobre este Vídeo'}
+        description={
+          'Introduza uma pequena introdução que descreva o core do vídeo. Na descrição pode incluir detalhes mais vastos.'
+        }
+      />
+      <FormInput label="Introdução" placeholder="" type="text" {...register('introduction')} />
+
+      {/* <FormRichTextEditor
+        label="Descrição"
+        placeholder="Introdução"
+        onChange={(value) => {
+          console.log(value)
+        }}
+      /> */}
+
+      {/* Metadata fields */}
+      <FormSection
+        title={'Metadados'}
+        description={'Escolha vários tópicos que se relacionam com o tema do seu vídeo.'}
+      />
+      <FormSelect
+        label="Área de Interesse"
+        placeholder="— Selecione uma opção —"
+        onChange={setSection}
+        data={[
+          { value: 'geral', label: 'Geral' },
+          {
+            value: 'colposcopia_patologia_tracto_genital_inferior',
+            label: 'Colposcopia Patologia Tracto Genital Inferior',
+          },
+          { value: 'endoscopia_ginecologica', label: 'Endoscopia Ginecológica' },
+          { value: 'ginecologia_oncologica', label: 'Ginecologia Oncológica' },
+          { value: 'menopausa', label: 'Menopausa' },
+          { value: 'uroginecologia', label: 'Uroginecologia' },
+        ]}
+      />
+      {/* Privacy and conflicts of interest fields */}
+      <FormSection
+        title={'Privacidade e Conflito de Interesses'}
+        description={
+          'A SPG valoriza os direitos de privacidade e a garantia de não existirem quaisquer conflitos de interesse inerentes à publicação de um vídeo científico. Pedimos ao autor do vídeo que assine a Declaração de Cedência de Direitos e faça o upload do ficheiro no campo seguinte. Pode encontrar a Declaração de Cedência de Direitos no link abaixo.'
+        }
+      />
+      <FormInput
+        label="Declaração Assinada"
+        placeholder=""
+        type="file"
+        {...register('declarationFile')}
+      />
+      <FormCheckbox
+        {...register('isPrivacyChecked', { required: true })}
+        text={
+          <>
+            Confirmo que possuo os direitos de imagem dos ficheiros enviados e estão ocultas as
+            respectivas identificações pessoais, de acordo com o{' '}
+            <a
+              target="_blank"
+              href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:02016R0679-20160504"
+            >
+              Regulamento Geral de Protecção de Dados (RGPD)
+            </a>
+            .
+          </>
+        }
+      />
+      <Button fullWidth type="submit" disabled={!watch('isPrivacyChecked')}>
+        Submeter Vídeo
+      </Button>
+    </form>
+  )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,28 +1,40 @@
-import { FieldHook } from "payload"
+import { FieldHook } from 'payload'
 
 export function slugify(text: string) {
-	return text
-		.replace(/ /g, '-')
-		.replace(/[^\w-/]+/g, '')
-		.toLowerCase()
+  return text
+    .replace(/ /g, '-')
+    .replace(/[^\w-/]+/g, '')
+    .toLowerCase()
 }
 
 export const formatSlug =
-	(fallback: string): FieldHook =>
-		({ value, originalDoc, data }) => {
-			if (typeof value === 'string') {
-				return slugify(value)
-			}
-			const fallbackData = data?.[fallback] || originalDoc?.[fallback]
+  (fallback: string): FieldHook =>
+  ({ value, originalDoc, data }) => {
+    if (typeof value === 'string') {
+      return slugify(value)
+    }
+    const fallbackData = data?.[fallback] || originalDoc?.[fallback]
 
-			if (fallbackData && typeof fallbackData === 'string') {
-				return slugify(fallbackData)
-			}
+    if (fallbackData && typeof fallbackData === 'string') {
+      return slugify(fallbackData)
+    }
 
-			return value
-		}
+    return value
+  }
 
 export function capitalizeDate(str: string) {
-	var reg = /\b([a-zÁ-ú]{3,})/g;
-	return str.replace(reg, (w) => w.charAt(0).toUpperCase() + w.slice(1));
+  var reg = /\b([a-zÁ-ú]{3,})/g
+  return str.replace(reg, (w) => w.charAt(0).toUpperCase() + w.slice(1))
+}
+
+export function generateId() {
+  var timestamp = ((new Date().getTime() / 1000) | 0).toString(16)
+  return (
+    timestamp +
+    'xxxxxxxxxxxxxxxx'
+      .replace(/[x]/g, function () {
+        return ((Math.random() * 16) | 0).toString(16)
+      })
+      .toLowerCase()
+  )
 }
