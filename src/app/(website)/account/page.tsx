9163bbb0c50ed49ebcb0357a37cb99d35e8b1147
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-11-03 12:33:28 +0000

    Refactor account page component

--- src/app/(website)/account/page.tsx
+++ src/app/(website)/account/page.tsx
@@ -1,14 +1,12 @@
-import Account from "@/components/account";
-import { getPayloadHMR } from "@payloadcms/next/utilities";
-import config from "@/payload.config"
+import Account from '@/components/account'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import config from '@/payload.config'
 
 export default async function Page() {
-    const payload = await getPayloadHMR({ config })
-	const account = await payload.findGlobal({
-		slug: 'account',
-	});
-    
-    return (
-        <Account account={account} title="Edite as suas informações" />
-    )
+  const payload = await getPayloadHMR({ config })
+  const account = await payload.findGlobal({
+    slug: 'account',
+  })
+
+  return <Account account={account} />
 }
