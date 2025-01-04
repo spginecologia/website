e5b67385ef9b7610330c26f1f9a2a6c8b372d97a
João de Vasconcelos <contact@joao.earth>
2025-01-04 23:05:36 +0000

    Refactor SectionDetail component to use slug for data fetching; add SectionDetailIntro for improved layout and content presentation

--- frontend/app/(views)/(website)/sections/[section_slug]/page.tsx
+++ frontend/app/(views)/(website)/sections/[section_slug]/page.tsx
@@ -5,6 +5,6 @@ import { SectionDetail } from '@/components/sections/SectionDetail';
 /* * */
 
 export default async function Page({ params }) {
-	const { section_id } = await params;
-	return <SectionDetail id={section_id} />;
+	const { section_slug } = await params;
+	return <SectionDetail slug={section_slug} />;
 }
---
+++ frontend/app/api/sections/[section_slug]/route.ts
@@ -0,0 +1,45 @@
+/* * */
+
+import payloadConfig from '@payload-config';
+import { getPayload } from 'payload';
+
+/* * */
+
+export async function GET(_: Request, { params }: { params: Promise<{ section_slug: string }> }) {
+	try {
+		//
+
+		//
+		// Get the invoice ID from the request query
+
+		const sectionSlug = (await params).section_slug;
+
+		if (!sectionSlug || typeof sectionSlug !== 'string') return new Response(null, { status: 400 });
+
+		//
+		// Search videos for the current user
+
+		const payload = await getPayload({ config: payloadConfig });
+
+		const foundSections = await payload.find({
+			collection: 'sections',
+			where: {
+				slug: {
+					equals: sectionSlug,
+				},
+			},
+		});
+
+		if (!foundSections || foundSections.docs.length === 0) {
+			return new Response(null, { status: 404 });
+		}
+
+		return Response.json(foundSections.docs[0]);
+
+		//
+	}
+	catch (err) {
+		console.log(err);
+		return Response.error();
+	}
+}
