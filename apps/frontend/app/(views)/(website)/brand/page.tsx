af8e6f5ba6838ae4dcc021abee91dd59d84dfa0c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-03-25 23:22:22 +0000

    Merge https://github.com/spginecologia/website/pull/3/files

---
+++ apps/frontend/app/(views)/(website)/brand/page.tsx
@@ -0,0 +1,17 @@
+/* * */
+
+import { BrandAssetsMain } from '@/components/brand/BrandAssetsMain';
+import { BrandColors } from '@/components/brand/BrandColors';
+import { BrandTypography } from '@/components/brand/BrandTypography';
+
+/* * */
+
+export default function Page() {
+	return (
+		<div>
+			<BrandAssetsMain />
+			<BrandColors />
+			<BrandTypography />
+		</div>
+	);
+}
