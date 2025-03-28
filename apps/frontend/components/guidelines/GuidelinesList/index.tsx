2681de6c3ca07ea82ddbfc69d2358b20aaf49a91
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-03-28 00:26:29 +0000

    Refactor GuidelinesList component to import Guideline type correctly and enhance grid responsiveness in styles

--- apps/frontend/components/guidelines/GuidelinesList/index.tsx
+++ apps/frontend/components/guidelines/GuidelinesList/index.tsx
@@ -2,14 +2,13 @@
 
 /* * */
 
-import type { Guideline } from '@/payload-types';
-
 import { Card } from '@/components/cards/Card';
 import { CardSkeleton } from '@/components/cards/CardSkeleton';
 import { ContentWrapper } from '@/components/common/ContentWrapper';
 import { ErrorDisplay } from '@/components/common/ErrorDisplay';
 import { NoDataDisplay } from '@/components/common/NoDataDisplay';
 import { Section } from '@/components/common/Section';
+import { type Guideline } from '@/payload-types';
 import { PayloadAPIResponse } from '@/types/payload-api-response';
 import { Title } from '@mantine/core';
 import { useTranslations } from 'next-intl';
