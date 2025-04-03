4db9a0086d650063709aa3831a233f1cf9d8cb73
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-04-04 00:40:44 +0100

    Refactor FooterNewsletter component to use environment variable for Turnstile site key and migrate configuration to TypeScript

---
+++ apps/frontend/next.config.ts
@@ -0,0 +1,37 @@
+/* * */
+
+import { withPayload } from '@payloadcms/next/withPayload';
+import { type NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+/* * */
+
+const nextConfig: NextConfig = {
+	env: {
+		NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY : '0x4AAAAAAA4UHAUCG1nzvjW5',
+		NEXT_PUBLIC_COOKIE_DOMAIN: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_COOKIE_DOMAIN : '.dev.spginecologia.pt',
+		NEXT_PUBLIC_URL: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_URL : 'https://dev.spginecologia.pt',
+	},
+	images: {
+		remotePatterns: [
+			{
+				hostname: 'spginecologia.pt',
+				port: '',
+				protocol: 'https',
+			},
+		],
+	},
+	output: 'standalone',
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			//
+		];
+	},
+};
+
+/* * */
+
+const withNextIntl = createNextIntlPlugin();
+
+export default withPayload(withNextIntl(nextConfig));
