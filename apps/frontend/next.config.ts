5f6ce5fca48b0aabd5227252b50a266465c39d9e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-04-04 00:59:34 +0100

    Update Turnstile site key in next.config.ts for production environment

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -8,7 +8,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
 	env: {
-		NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY : '0x4AAAAAAA4UHAUCG1nzvjW5',
+		NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY : '0x4AAAAAAASWlsU2RF7qF_zi',
 		NEXT_PUBLIC_COOKIE_DOMAIN: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_COOKIE_DOMAIN : '.dev.spginecologia.pt',
 		NEXT_PUBLIC_URL: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_URL : 'https://dev.spginecologia.pt',
 	},
