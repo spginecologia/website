b706fef2eb653264636ea9fb55f97556da7de4b0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-01-04 17:42:31 +0000

    Update packages

--- apps/frontend/src/app/(payload)/admin/importMap.js
+++ apps/frontend/src/app/(payload)/admin/importMap.js
@@ -25,6 +25,7 @@ import { SpgMemberRowLabel as SpgMemberRowLabel_c6a59ba124efbe19240b0bf5154fe8b4
 import { FileOrUrlRowLabel as FileOrUrlRowLabel_e392e86270e94bf15012006ccac0b5ce } from '@/services/payload/components/FileOrUrlRowLabel/index'
 import { OpenTransactionDocumentButton as OpenTransactionDocumentButton_ff581ccc4f936cf6b07007539c523b86 } from '@/services/payload/components/OpenTransactionDocumentButton/index'
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
+import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from '@payloadcms/ui/rsc'
 
 export const importMap = {
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
@@ -53,5 +54,6 @@ export const importMap = {
   "@/services/payload/components/SpgMemberRowLabel/index#SpgMemberRowLabel": SpgMemberRowLabel_c6a59ba124efbe19240b0bf5154fe8b4,
   "@/services/payload/components/FileOrUrlRowLabel/index#FileOrUrlRowLabel": FileOrUrlRowLabel_e392e86270e94bf15012006ccac0b5ce,
   "@/services/payload/components/OpenTransactionDocumentButton/index#OpenTransactionDocumentButton": OpenTransactionDocumentButton_ff581ccc4f936cf6b07007539c523b86,
-  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
+  "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a
 }
