9680eefbb6b4586aa7b039ce666eb2413f03b4f3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-05 20:27:18 +0000

    Cleanup

--- apps/frontend/payload-types.ts
+++ apps/frontend/payload-types.ts
@@ -126,9 +126,7 @@ export interface Config {
     'social-bodies': SocialBodiesSelect<false> | SocialBodiesSelect<true>;
   };
   locale: null;
-  user: User & {
-    collection: 'users';
-  };
+  user: User;
   jobs: {
     tasks: unknown;
     workflows: unknown;
@@ -494,6 +492,7 @@ export interface User {
       }[]
     | null;
   password?: string | null;
+  collection: 'users';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
