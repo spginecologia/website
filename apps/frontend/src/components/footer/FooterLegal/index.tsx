4f95e92357712d2e39bbc68efc7a37ab3720217d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-07 13:24:41 +0000

    Add version number in footer

--- apps/frontend/src/components/footer/FooterLegal/index.tsx
+++ apps/frontend/src/components/footer/FooterLegal/index.tsx
@@ -2,6 +2,7 @@
 
 /* * */
 
+import pjson from '@pjson';
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 
@@ -31,6 +32,9 @@ export function FooterLegal() {
 				<p className={styles.copyright}>{t('footer.FooterLegal.copyright', { year: new Date().getFullYear() })}</p>
 			</div>
 			<div className={styles.right}>
+				<Link className={styles.legalLink} href="http://github.com/spginecologia/website" target="_blank">
+					{pjson.version}
+				</Link>
 				<Link className={styles.legalLink} href="/brand" target="_blank">
 					{t('footer.FooterLegal.brand_assets')}
 				</Link>
