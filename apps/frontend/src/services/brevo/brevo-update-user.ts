de2247d285e2a44b99086e08ba362bf884430399
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-10 10:55:22 +0000

    Update Brevo identifiers

--- apps/frontend/src/services/brevo/brevo-update-user.ts
+++ apps/frontend/src/services/brevo/brevo-update-user.ts
@@ -7,7 +7,7 @@ import { type User } from 'payload-types';
  * Updates the user's attributes, including newsletter subscription, in Brevo.
  * @param userData User data to be updated in Brevo.
  */
-export async function brevoUpdateUser(userData: User, updateBy: 'email' | 'id') {
+export async function brevoUpdateUser(userData: User, updateBy: 'email' | 'tax_id') {
 	//
 
 	//
@@ -22,16 +22,25 @@ export async function brevoUpdateUser(userData: User, updateBy: 'email' | 'id') 
 		requestPath = undefined;
 		requestMethod = 'POST';
 	}
-	else if (updateBy === 'id') {
-		// If we use id, then we must use a PUT as it will
+	else if (updateBy === 'tax_id') {
+		// If we use tax_id, then we must use a PUT as it will
 		// update an existing user. This happens when the user
 		// changes their email, for example.
-		requestPath = `${userData.id}?identifierType=ext_id`;
+		requestPath = `${userData.tax_id}?identifierType=ext_id`;
 		requestMethod = 'PUT';
 	}
 	else {
-		throw new Error('Invalid updateBy parameter. Use "email" or "id".');
+		throw new Error('Invalid updateBy parameter. Use "email" or "tax_id".');
 	}
+
+	//
+	// Detect which newsletter lists to subscribe/unsubscribe
+
+	const listsSubscribed: number[] = [10];
+	const listsUnsubscribed: number[] = [];
+
+	if (userData.send_newsletter === true) listsSubscribed.push(11);
+	else listsUnsubscribed.push(11);
 
 	//
 	// Send the updated user data to Brevo
@@ -40,13 +49,15 @@ export async function brevoUpdateUser(userData: User, updateBy: 'email' | 'id') 
 		data: JSON.stringify({
 			attributes: {
 				EMAIL: userData.email,
-				EXT_ID: userData.id,
+				EXT_ID: userData.tax_id,
 				FIRSTNAME: userData.first_name,
 				LASTNAME: userData.last_name,
 				TITLE: userData.title,
 			},
 			email: userData.email,
-			emailBlacklisted: userData.send_newsletter === false ? true : false,
+			emailBlacklisted: false,
+			listIds: listsSubscribed,
+			unlinkListIds: listsUnsubscribed,
 			updateEnabled: true,
 		}),
 		method: requestMethod,
--- apps/frontend/src/services/payload/collections/User/actions/update-brevo.ts
+++ apps/frontend/src/services/payload/collections/User/actions/update-brevo.ts
@@ -14,12 +14,38 @@ export const updateBrevo: CollectionAfterChangeHook<User> = async ({ doc, previo
 	//
 
 	//
-	// If the user has changed their email,
-	// then we need to update the user in Brevo using ext_id value.
+	// If the user has changed their email, first_name, last_name,
+	// title or send_newsletter fields, then we need to update
+	// the user in Brevo using ext_id value.
 
 	if (previousDoc.email && previousDoc.email !== doc.email) {
 		console.log('User changed email:', previousDoc.email, '->', doc.email);
-		await brevoUpdateUser(doc, 'id');
+		await brevoUpdateUser(doc, 'tax_id');
+		return;
+	}
+
+	if (previousDoc.title && previousDoc.title !== doc.title) {
+		console.log('User changed title:', previousDoc.title, '->', doc.title);
+		await brevoUpdateUser(doc, 'email');
+		return;
+	}
+
+	if (previousDoc.first_name && previousDoc.first_name !== doc.first_name) {
+		console.log('User changed first name:', previousDoc.first_name, '->', doc.first_name);
+		await brevoUpdateUser(doc, 'email');
+		return;
+	}
+
+	if (previousDoc.last_name && previousDoc.last_name !== doc.last_name) {
+		console.log('User changed last name:', previousDoc.last_name, '->', doc.last_name);
+		await brevoUpdateUser(doc, 'email');
+		return;
+	}
+
+	if (previousDoc.send_newsletter && previousDoc.send_newsletter !== doc.send_newsletter) {
+		console.log('User changed last name:', previousDoc.send_newsletter, '->', doc.send_newsletter);
+		await brevoUpdateUser(doc, 'email');
+		return;
 	}
 
 	//
