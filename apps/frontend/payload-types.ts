df3100440b4affca7cb81a959517c3ce32ad9bb4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-02 22:52:51 +0000

    feat: add CollectionsWidget interface and update Config to include widgets

--- apps/frontend/payload-types.ts
+++ apps/frontend/payload-types.ts
@@ -128,6 +128,9 @@ export interface Config {
     'social-bodies': SocialBodiesSelect<false> | SocialBodiesSelect<true>;
   };
   locale: null;
+  widgets: {
+    collections: CollectionsWidget;
+  };
   user: User;
   jobs: {
     tasks: unknown;
@@ -1327,6 +1330,16 @@ export interface SocialBodiesSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
   globalType?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "collections_widget".
+ */
+export interface CollectionsWidget {
+  data?: {
+    [k: string]: unknown;
+  };
+  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
