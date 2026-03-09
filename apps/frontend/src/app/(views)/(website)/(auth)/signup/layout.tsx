4107b5e6d47a0c2f0f8b15bab196347c0c0c388a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-09 22:50:20 +0000

    Refactor signup components: move SignupAd and SignupGuide to new 'signup' directory; update imports accordingly. Remove old auth components and styles.

--- apps/frontend/src/app/(views)/(website)/(auth)/signup/layout.tsx
+++ apps/frontend/src/app/(views)/(website)/(auth)/signup/layout.tsx
@@ -1,7 +1,7 @@
 /* * */
 
 import { AuthWrapper } from '@/components/auth/AuthWrapper';
-import { SignupGuide } from '@/components/auth/SignupGuide';
+import { SignupGuide } from '@/components/signup/SignupGuide';
 
 /* * */
 
