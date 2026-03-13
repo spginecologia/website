3db8b16e57595ed43919afbb12f9a57fe65ffae1
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-13 13:53:36 +0000

    feat: reset default values in SignupFormDefault for a cleaner initial state

--- apps/frontend/src/services/payload/collections/Signup/default.ts
+++ apps/frontend/src/services/payload/collections/Signup/default.ts
@@ -1,6 +1,6 @@
 /* * */
 
-import { SignupForm } from '@/services/payload/collections/Signup/validation';
+import { type SignupForm } from '@/services/payload/collections/Signup/validation';
 
 /* * */
 
@@ -13,23 +13,22 @@ export const SignupFormDefault: SignupForm = {
 	billing_name: '',
 	billing_postal_code: '',
 	billing_tax_id: '',
-	birthday: '2025-01-01',
+	birthday: '',
 	city: '',
 	country: 'Portugal',
-	email: 'spg-proponente@joao.earth',
+	email: '',
 	enrolment_sponsors: [
-		{ is_valid: true, tax_id: '233144455' },
-		{ is_valid: true, tax_id: '100336574' },
+		{ is_valid: false, tax_id: '' },
 	],
-	first_name: 'Proponente 1',
-	last_name: 'Apelido',
-	medical_id: '1234',
-	medical_specialty: 'gynecology',
-	phone: '123456789',
+	first_name: '',
+	last_name: '',
+	medical_id: '',
+	medical_specialty: undefined,
+	phone: '',
 	postal_code: '',
 	send_newsletter: true,
 	subscribed_sections: [],
-	tax_id: '295365951',
+	tax_id: '',
 	title: null,
 	workplace_primary: '',
 	workplace_secondary: '',
